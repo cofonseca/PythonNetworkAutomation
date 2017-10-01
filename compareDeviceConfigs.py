aa2dc9fbae2b89647e3d0e3e53f66f8a1857bb75
Corey Fonseca <cfonseca@blueport.com>
2017-10-01 18:43:37 -0400

    Initial Commit.

---
+++ compareDeviceConfigs.py
@@ -0,0 +1,40 @@
+import napalm
+import json
+import time
+import os
+import sys
+
+ip_address = sys.argv[1]
+configFile = sys.argv[2]
+
+if len(sys.argv) != 3:
+    print("Usage: compareDeviceConfigs.py <IP Address> <Config.txt>")
+
+netDevices = json.load(open('devices.json'))
+for netDevice in netDevices:
+    if netDevice['ip_address'] == ip_address:
+        if netDevice['device_type'] == 'cisco_ios':
+            driver = napalm.get_network_driver('ios')
+            device = driver(hostname=netDevice['ip_address'], username=netDevice['username'], password=netDevice['password'], timeout=10, optional_args={'secret': netDevice['secret']})
+        elif netDevice['device_type'] == 'fortinet':
+            driver = napalm.get_network_driver('fortios')
+            device = driver(hostname=netDevice['ip_address'], username=netDevice['username'], password=netDevice['password'], timeout=10)
+        else:
+            print('Unknown Device Type')
+        # Begin Connection
+        try:
+            print('Connecting to ' + netDevice['ip_address'])
+            device.open()
+            print('Loading Configuration...')
+            device.load_merge_candidate(configFile)
+            print('Comparing Configuration...')
+            diffs = device.compare_config()
+            if len(diffs) > 0:
+                print('Differences:')
+                print(diffs)
+            device.discard_config()
+            device.close()
+        except:
+            print('Failed to connect to ' + netDevice['ip_address'])
+
+# https://napalm.readthedocs.io/en/latest/base.html
